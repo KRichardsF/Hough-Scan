164bb215bf5402ccf95251fe475e8330ed73c93a
Kyle Richards <KRichardsF@gmail.com>
2025-03-01 17:41:02 +0000

    Bound tiling parameters.

--- main.py
+++ main.py
@@ -161,6 +161,7 @@ def home():
                             'Tile Size',
                             current_value=current_settings.active_parameters.tile_size,
                             maximum_value=6000,
+                            minimum_value=0,
                             id="tile-size-input",
                             hx_post="/parameter-updated",
                             hx_trigger="change, click",
@@ -168,8 +169,30 @@ def home():
                             hx_swap="none",
                             **{':hx-vals': 'JSON.stringify({ val: currentVal, name:"tile_size" })'}
                         ),
-                        entry.number_input('Tile Overlap', current_value=current_settings.active_parameters.tile_overlap),
-                        entry.number_input('Doubles removal distance', current_value=current_settings.active_parameters.doubles_removal_distance),
+                        entry.number_input(
+                            'Tile Overlap', 
+                            current_value=current_settings.active_parameters.tile_overlap,
+                            maximum_value=2000,
+                            minimum_value=0,
+                            id="overlap-input",
+                            hx_post="/parameter-updated",
+                            hx_trigger="change, click",
+                            hx_target="this",
+                            hx_swap="none",
+                            **{':hx-vals': 'JSON.stringify({ val: currentVal, name:"tile_overlap" })'}
+                            ),
+                        entry.number_input(
+                            'Doubles removal distance', 
+                            current_value=current_settings.active_parameters.doubles_removal_distance,
+                            maximum_value=2000,
+                            minimum_value=0,
+                            id="doubles-removal-input",
+                            hx_post="/parameter-updated",
+                            hx_trigger="change, click",
+                            hx_target="this",
+                            hx_swap="none",
+                            **{':hx-vals': 'JSON.stringify({ val: currentVal, name:"doubles_removal_distance" })'}
+                            ),
                         cls="flex flex-row h-40 items-center justify-evenly",
                     ),
                     cls="flex flex-col",
@@ -521,7 +544,10 @@ def image_returned_success():
 def process_image():
     print("Processing image...")
     all_cirlces_overlay = []
-    tile_processor = TileProcessor(np.array(current_settings.image.convert('L')))
+    tile_processor = TileProcessor(np.array(current_settings.image.convert('L')), 
+                                   tile_size=current_settings.active_parameters.tile_size, 
+                                   overlap=current_settings.active_parameters.tile_overlap)
+
     for i in current_settings.scan_parameters:
         circles = []
         detected_circles = tile_processor.process_tiles_parallel(
